ce25aa1469aa19cc72c4cbbaa24e5c67ed04b519
ekramul <ekramulshawon1@gmail.com>
2024-05-27 08:50:14 +0600

    initial commit

---
+++ resources/views/frontend/js/js.blade.php
@@ -0,0 +1,17 @@
+<script src="{{ asset('assets') }}/js/jquery-1.12.4.min.js"></script>
+<script src="{{ asset('assets') }}/js/bootstrap.min.js"></script>
+<script src="{{ asset('assets') }}/js/chosen.min.js"></script>
+<script src="{{ asset('assets') }}/js/countdown.min.js"></script>
+<script src="{{ asset('assets') }}/js/jquery.scrollbar.min.js"></script>
+<script src="{{ asset('assets') }}/js/lightbox.min.js"></script>
+<script src="{{ asset('assets') }}/js/magnific-popup.min.js"></script>
+<script src="{{ asset('assets') }}/js/slick.js"></script>
+<script src="{{ asset('assets') }}/js/jquery.zoom.min.js"></script>
+<script src="{{ asset('assets') }}/js/threesixty.min.js"></script>
+<script src="{{ asset('assets') }}/js/jquery-ui.min.js"></script>
+<script src="{{ asset('assets') }}/js/mobilemenu.js"></script>
+<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyC3nDHy1dARR-Pa_2jjPCjvsOR4bcILYsM'></script>
+<script src="{{ asset('assets') }}/js/functions.js"></script>
+
+
+@stack('frontend_js')
